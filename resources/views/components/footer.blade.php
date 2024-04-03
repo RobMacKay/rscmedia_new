8695b7a8f154c0c6b82ac053915051b7ba7de133
Rob MacKay <robskiwarrior@gmail.com>
2024-04-03 13:11:53 +0100

    Add footer component and update guest layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,68 @@
+<footer class="mt-10 p-10 pb-16 md:pt-20">
+    <div class="container mx-auto flex max-w-6xl flex-col">
+        <div class="flex w-full flex-col !items-center justify-between gap-4 md:flex-row">
+            <div class="flex flex-col">
+                <h6
+                    class="mb-3 block !text-left font-sans text-base font-semibold leading-relaxed tracking-normal text-inherit antialiased">
+                    RSC Media
+                </h6>
+                <div class="flex flex-wrap items-center gap-5">
+                    <ul class="">
+                        <li>
+                            <a href="/"
+                                class="block font-sans text-base font-normal leading-relaxed !text-gray-700 antialiased transition-colors hover:!text-gray-700">Home</a>
+                        </li>
+                    </ul>
+                    <ul class="">
+                        <li>
+                            <a href="/about"
+                                class="block font-sans text-base font-normal leading-relaxed !text-gray-700 antialiased transition-colors hover:!text-gray-700">About
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="">
+                        <li>
+                            <a href="/services"
+                                class="block font-sans text-base font-normal leading-relaxed !text-gray-700 antialiased transition-colors hover:!text-gray-700">Services</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="flex flex-col items-end">
+                <h6
+                    class="mb-4 block w-full text-left font-sans text-base font-semibold leading-relaxed tracking-normal text-inherit antialiased md:!text-right lg:w-7/12">
+                    Success is not final, failure is not fatal: It is the courage to continue that counts
+                </h6>
+                <div class="flex w-full gap-2 sm:w-fit">
+                    <button
+                        class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-primary/10 active:bg-primary/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="button" data-ripple-dark="true">
+                        <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><i
+                                class="fa-brands fa-twitter text-lg"></i></span></button><button
+                        class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-primary/10 active:bg-primary/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="button" data-ripple-dark="true">
+                        <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><i
+                                class="fa-brands fa-youtube text-lg"></i></span></button><button
+                        class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-primary/10 active:bg-primary/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="button" data-ripple-dark="true">
+                        <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><i
+                                class="fa-brands fa-instagram text-lg"></i></span></button><button
+                        class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-primary/10 active:bg-primary/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        type="button" data-ripple-dark="true">
+                        <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><i
+                                class="fa-brands fa-github text-lg"></i></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div>
+            <p
+                class="ray-900 mt-3 block text-left font-sans !text-sm font-normal leading-relaxed text-gray-700 antialiased lg:mt-0">
+                All rights reserved. &copy;
+                <!-- -->{{date('Y')}}
+                <!-- -->
+                RSC Media Ltd
+            </p>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,21 +7,45 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <script defer src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@700&display=swap" rel="stylesheet">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- OpenGraph meta tags -->
+        <meta property="og:title" content="RSC Media - Website Development & Support">
+        <meta property="og:description" content="Discover the power of RSC Media for comprehensive web solutions. From development to hosting, consultancy, and copywriting, we've got you covered. Elevate your digital presence today!">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://rscmedia.co.uk">
+        <meta property="og:image" content="https://rscmedia.co.uk/assets/images/facebook-cover.jpg">
+        <meta property="og:image:alt" content="RSC Media Logo">
+        <meta property="og:site_name" content="RSC Media">
+
+        <!-- Twitter Card meta tags -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="RSC Media - Website Development & Support">
+        <meta name="twitter:description" content="Unlock the potential of your online presence with RSC Media. Web development, hosting, consultancy, and copywriting - we're your one-stop shop for digital success!">
+        <meta name="twitter:image" content="https://rscmedia.co.uk/assets/images/facebook-cover.jpg">
 
         <!-- Styles -->
         @livewireStyles
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="antialiased">
             {{ $slot }}
         </div>
-
+        <x-footer />
+        @include('whatsappbutton')
         @livewireScripts
     </body>
 </html>
